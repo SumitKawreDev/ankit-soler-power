8440935f444d97b4c69bb1ca64308600e4fa3569
SumitKawreDev <sumitkawre1999@gmail.com>
2026-04-01 13:38:14 +0530

    social meadia profile link updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             <div className="flex gap-3">
               {/* Facebook */}
               <a
-                href="https://facebook.com"
+                href="https://www.facebook.com/share/18NtFFPCSN/?mibextid=wwXIfr"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 bg-[#023d2a] rounded-lg flex items-center justify-center transition-all duration-300 hover:scale-110 hover:bg-[#035238] group"
@@ -37,7 +37,7 @@ export default function Footer() {
 
               {/* Instagram */}
               <a
-                href="https://instagram.com"
+                href="https://www.instagram.com/ankit_solar_solutions_?igsh=Z2M5bW82ejR0djRh"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 bg-[#023d2a] rounded-lg flex items-center justify-center transition-all duration-300 hover:scale-110 hover:bg-[#035238] group"
@@ -49,7 +49,7 @@ export default function Footer() {
               </a>
 
               {/* Twitter (X) */}
-              <a
+              {/* <a
                 href="https://twitter.com"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -59,10 +59,10 @@ export default function Footer() {
                 <svg className="w-5 h-5 text-white transition-colors duration-300 group-hover:text-green-300" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                 </svg>
-              </a>
+              </a> */}
 
               {/* YouTube */}
-              <a
+              {/* <a
                 href="https://youtube.com"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -72,7 +72,7 @@ export default function Footer() {
                 <svg className="w-5 h-5 text-white transition-colors duration-300 group-hover:text-green-300" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
                 </svg>
-              </a>
+              </a> */}
             </div>
           </div>
 
@@ -133,7 +133,7 @@ export default function Footer() {
                 </div>
                 <div>
                   <p className="text-xs text-green-400">Phone</p>
-                  <a href="tel:+919876543210" className="text-green-200 hover:text-accent text-sm transition-colors">+91 9479369947</a>
+                  <a href="tel:+919479369947" className="text-green-200 hover:text-accent text-sm transition-colors">+91 9479369947</a>
                 </div>
               </li>
               <li className="flex items-start gap-3">
